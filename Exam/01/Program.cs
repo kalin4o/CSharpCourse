d60125bc5de5380f082fb4399a1916c9f7471f67
kalin4o <piiz69@yahoo.com>
2013-02-15 21:05:54 +0200

    C# Part 2 Exam

---
+++ Exam/01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class ProvadiaNumbers
+{
+    static void Main()
+    {
+        ulong number = ulong.Parse(Console.ReadLine());
+        List<ulong> result = new List<ulong>();
+        while (number > 0)
+        {
+            result.Add(number % 256);
+            number = number / 256;
+        }
+        result.Reverse();
+        foreach (var item in result)
+        {
+            ulong check = item / 26;
+            if (check > 0)
+            {
+                Console.Write((char)(96 + check));
+            }
+            Console.Write((char)(item + 65 - (26*check)));
+        }
+    }
+}
---
+++ Exam/02/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Midget
+{
+    static int Sum(int[] valley, int[] pattern, int index, int sum, List<int> indexes)
+    {
+
+
+        for (int i = 0; i < pattern.Length; i++)
+        {
+            index += pattern[i];
+            if (index >= valley.Length || index < 0)
+            {
+                return sum;
+            }
+            else
+            {
+                if (indexes.Contains(index))
+                {
+                    return sum;
+                }
+                else
+                {
+                    indexes.Add(index);
+                }
+                sum += valley[index];
+            }
+        }
+
+        return Sum(valley, pattern, index, sum, indexes);
+
+    }
+
+
+
+    static int[] StringToInt(string numbers)
+    {
+        string[] newString = numbers.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+        int[] result = new int[newString.Length];
+        for (int i = 0; i < result.Length; i++)
+        {
+            result[i] = int.Parse(newString[i]);
+        }
+        return result;
+
+    }
+    static void Main()
+    {
+        string theValley = Console.ReadLine(); //"1, 3, -6, 7, 4, 1, 12"
+        int[] valley = StringToInt(theValley);
+        int bestSum = int.MinValue;
+        int numberOfPatterns = int.Parse(Console.ReadLine());
+        string[] patterns = new string[numberOfPatterns];
+
+        List<int> indexes = new List<int> { 0 };
+        for (int i = 0; i < patterns.Length; i++)
+        {
+
+            patterns[i] = Console.ReadLine();
+            int sum = Sum(valley, StringToInt(patterns[i]), 0, valley[0], indexes);
+            indexes.Clear();
+            if (sum > bestSum)
+            {
+                bestSum = sum;
+            }
+        }
+        Console.WriteLine(bestSum);
+    }
+}
